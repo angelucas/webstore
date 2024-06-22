328c86fe2553fbcadf62ae11811efee4ed7e7ba0
Lucas M. Angelo <angelucas@gmail.com>
2024-06-21 22:55:00 -0300

    Add Models

---
+++ MinimalAPI/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace MinimalAPI.Models
+{
+    public class TodoItem
+    {
+        public int Int { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
