23ce893500b6ac7e4757305247ee1b29bb76b4e9
Lucas M. Angelo <angelucas@gmail.com>
2024-06-22 03:22:18 -0300

    Recreate the project

--- MinimalAPI/Models/TodoItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MinimalAPI.Models
-{
-    public class TodoItem
-    {
-        public int Int { get; set; }
-        public string Name { get; set; }
-        public bool IsComplete { get; set; }
-    }
-}
--- MinimalAPI/Program.cs
+++
@@ -1,9 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-
-app.Run();
