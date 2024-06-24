cec6ad07491fb0638019c3d8afbed00b644eaeb0
Lucas M. Angelo <angelucas@gmail.com>
2024-06-24 04:28:42 -0300

    Add GetProductsEndpoints.cs and GetProductsHandler.cs

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoints.cs
@@ -0,0 +1,23 @@
+﻿namespace Catalog.API.Products.GetProducts
+{
+    public record GetProductsResponse(IEnumerable<Product> Products);
+    public class GetProductsEndpoints : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products", async (ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductsQuery());
+
+                var response = result.Adapt<GetProductsResponse>();
+                
+                return Results.Ok(response);
+            })
+            .WithName("GetProducts")
+            .Produces<GetProductsResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Product")
+            .WithDescription("Get Product");
+        }
+    }
+}
