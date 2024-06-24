bba3047d9eaac89e7d40411052d7b3703e424e23
Lucas M. Angelo <angelucas@gmail.com>
2024-06-24 04:47:47 -0300

    Add custom exception class - ProductNotFoundException.cs

---
+++ src/Services/Catalog/Catalog.API/Exceptions/ProductNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalog.API.Exceptions
+{
+    public class ProductNotFoundException : Exception
+    {
+        public ProductNotFoundException() : base("Product not found!")
+        {
+        }
+    }
+}
