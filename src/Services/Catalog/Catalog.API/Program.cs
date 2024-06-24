e1a070e99f83a1f26938d430bb0adf47bb68a27a
Lucas M. Angelo <angelucas@gmail.com>
2024-06-24 04:10:18 -0300

    Add Carter and MediatR to Program.cs

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,15 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddCarter();
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
 });
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 app.MapCarter();
 
 app.Run();
