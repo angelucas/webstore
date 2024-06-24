d1bf6c14f5c2e7a97b232dbc4472b73eeb6cf81e
Lucas M. Angelo <angelucas@gmail.com>
2024-06-24 04:13:36 -0300

    Fix misspelled word

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -8,7 +8,7 @@
     {
         public void AddRoutes(IEndpointRouteBuilder app)
         {
-            app.MapPost("/prducts",
+            app.MapPost("/products",
                 async (CreateProductRequest request, ISender sender) =>
             {
                 var command = request.Adapt<CreateProductCommand>();
