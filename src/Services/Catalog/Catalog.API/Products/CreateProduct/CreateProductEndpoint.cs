57412c822d68a9eccff16ab1951029d8fd3b04e9
Lucas M. Angelo <angelucas@gmail.com>
2024-06-23 21:19:26 -0300

    Add CreateProductHandler.cs and CreateProductEndpoint.cs

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+    public record CreateProductResponse(Guid id);
+
+    public class CreateProductEndpoint
+    {
+    }
+}
