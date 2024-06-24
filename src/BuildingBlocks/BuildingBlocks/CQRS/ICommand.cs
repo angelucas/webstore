2f009d7dabf1d3cb9205f5faaee590a6f7a801b3
Lucas M. Angelo <angelucas@gmail.com>
2024-06-23 21:26:09 -0300

    Add ICommand.cs

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    public interface ICommand : ICommand<Unit>
+    {
+
+    }
+    public interface ICommand<out TResponse> : IRequest<TResponse>
+    {
+    }
+}
