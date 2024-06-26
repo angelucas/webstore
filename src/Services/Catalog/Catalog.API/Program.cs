2a13e4fe941f5246489ccdc83566f0ff4cdb45cf
Lucas M. Angelo <angelucas@gmail.com>
2024-06-25 23:32:38 -0300

    Add new service: Fluent Validation

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -5,6 +5,9 @@ builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
 });
+
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+
 builder.Services.AddMarten(opts =>
 {
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
