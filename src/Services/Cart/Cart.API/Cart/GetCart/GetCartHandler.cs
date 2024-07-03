7a50f416298d299a2f0b2eceff527ee3afd14686
Lucas M. Angelo <angelucas@gmail.com>
2024-07-02 23:26:30 -0300

    Add Get cart handler

---
+++ src/Services/Cart/Cart.API/Cart/GetCart/GetCartHandler.cs
@@ -0,0 +1,12 @@
+﻿namespace Cart.API.Cart.GetCart
+{
+    public record GetCartQuery(string UserName) : IQuery<GetCartResult>;
+    public record GetCartResult(ShoppingCart Cart);
+    public class GetCartQueryHandler : IQueryHandler<GetCartQuery, GetCartResult>
+    {
+        public async Task<GetCartResult> Handle(GetCartQuery request, CancellationToken cancellationToken)
+        {
+            return new GetCartResult(new ShoppingCart("swn"));
+        }
+    }
+}
