743c55b7adb9fb2526eefde1297ba15bb157dbc9
Lucas M. Angelo <angelucas@gmail.com>
2024-07-02 23:27:09 -0300

    Add Store cart handler

---
+++ src/Services/Cart/Cart.API/Cart/StoreCart/StoreCartHandler.cs
@@ -0,0 +1,22 @@
+﻿namespace Cart.API.Cart.StoreCart
+{
+    public record StoreCartCommand(ShoppingCart Cart) : ICommand<StoreCartResult>;
+    public record StoreCartResult(string UserName);
+    public class StoreCartCommandValidator : AbstractValidator<StoreCartCommand>
+    {
+        public StoreCartCommandValidator() 
+        {
+            RuleFor(x => x.Cart).NotNull().WithMessage("Cart can not be null");
+            RuleFor(x => x.Cart.UserName).NotEmpty().WithName("UserName is required");
+        }
+    }
+    public class StoreCartCommandHandler : ICommandHandler<StoreCartCommand, StoreCartResult>
+    {
+        public async Task<StoreCartResult> Handle(StoreCartCommand command, CancellationToken cancellationToken)
+        {
+            ShoppingCart cart = command.Cart;
+
+            return new StoreCartResult("swn");
+        }
+    }
+}
