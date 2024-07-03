c9f816f455ee2cdd23d8768b5019c3142842691b
Lucas M. Angelo <angelucas@gmail.com>
2024-07-02 23:26:10 -0300

    Add Get cart endpoint

---
+++ src/Services/Cart/Cart.API/Cart/GetCart/GetCartEndpoint.cs
@@ -0,0 +1,23 @@
+﻿namespace Cart.API.Cart.GetCart
+{
+    public record GetCartResponse(ShoppingCart Cart);
+    public class GetCartEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/carrinho/{userName}", async (string userName, ISender sender) =>
+            {
+                var result = await sender.Send(new GetCartQuery(userName));
+
+                var response = result.Adapt<GetCartResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("GetProductById")
+            .Produces<GetCartResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Product By Id")
+            .WithDescription("Get Product By Id");
+        }
+    }
+}
