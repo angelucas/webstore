997e7dd8f1216aebd958c94b583af8a3f8eb19f3
Lucas M. Angelo <angelucas@gmail.com>
2024-07-03 22:38:47 -0300

    - Add Cart.API cart repository

---
+++ src/Services/Cart/Cart.API/Data/CartRepository.cs
@@ -0,0 +1,27 @@
+﻿namespace Cart.API.Data
+{
+    public class CartRepository(IDocumentSession session) 
+        : ICartRepository
+    {
+        public async Task<ShoppingCart> GetCart(string userName, CancellationToken cancellationToken = default)
+        {
+            var cart = await session.LoadAsync<ShoppingCart>(userName, cancellationToken);
+
+            return cart is null ? throw new CartNotFoundException(userName) : cart;
+        }
+
+        public async Task<ShoppingCart> StoreCart(ShoppingCart cart, CancellationToken cancellationToken = default)
+        {
+            session.Store(cart);
+            await session.SaveChangesAsync(cancellationToken);
+            return cart;
+        }
+
+        public async Task<bool> DeleteCart(string userName, CancellationToken cancellationToken = default)
+        {
+            session.Delete<ShoppingCart>(userName);
+            await session.SaveChangesAsync(cancellationToken);
+            return true;
+        }
+    }
+}
---
+++ src/Services/Cart/Cart.API/Data/ICartRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Cart.API.Data
+{
+    public interface ICartRepository
+    {
+        Task<ShoppingCart> GetCart(string userName, CancellationToken cancellationToken = default);
+        Task<ShoppingCart> StoreCart(ShoppingCart cart, CancellationToken cancellationToken = default);
+        Task<bool> DeleteCart(string userName, CancellationToken cancellationToken = default);
+    }
+}
