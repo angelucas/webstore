1a27931d9b8a6a73666bc294b1a8a5403fa36d74
Lucas M. Angelo <angelucas@gmail.com>
2024-07-03 23:02:30 -0300

    - Add Cart.API cart not found exception

---
+++ src/Services/Cart/Cart.API/Exceptions/CartNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace Cart.API.Exceptions
+{
+    public class CartNotFoundException : NotFoundException
+    {
+        public CartNotFoundException(string userName) : base("Carrinho", userName)
+        {
+
+        }
+    }
+}
