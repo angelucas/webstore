2e23c538540492c08d856500a932db95968de18c
Lucas M. Angelo <angelucas@gmail.com>
2024-07-03 22:19:31 -0300

    - Add Cart.API delete cart handler

--- src/Services/Cart/Cart.API/Cart/DeleteCart/DeleteCartHandler.cs
+++ src/Services/Cart/Cart.API/Cart/DeleteCart/DeleteCartHandler.cs
@@ -1,6 +1,25 @@
-﻿namespace Cart.API.Cart.DeleteCart
+﻿
+namespace Cart.API.Cart.DeleteCart
 {
-    public class DeleteCartHandler
+    public record DeleteCartCommand(string UserName) : ICommand<DeleteCartResult>;
+    public record DeleteCartResult(bool IsSuccess);
+
+    public class DeleteCartCommandValidator : AbstractValidator<DeleteCartCommand>
     {
+        public DeleteCartCommandValidator()
+        {
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("UserName is required");
+        }
+    }
+
+    public class DeleteCartCommandHandler(ICartRepository repository)
+        : ICommandHandler<DeleteCartCommand, DeleteCartResult>
+    {
+        public async Task<DeleteCartResult> Handle(DeleteCartCommand command, CancellationToken cancellationToken)
+        {
+            await repository.DeleteCart(command.UserName, cancellationToken);
+
+            return new DeleteCartResult(true);
+        }
     }
 }
