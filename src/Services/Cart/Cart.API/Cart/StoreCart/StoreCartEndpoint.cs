b482ab4d69fcf06d622becdab1482d6f087247aa
Lucas M. Angelo <angelucas@gmail.com>
2024-07-03 22:22:10 -0300

    - Add Cart.API store cart endpoint

--- src/Services/Cart/Cart.API/Cart/StoreCart/StoreCartEndpoint.cs
+++ src/Services/Cart/Cart.API/Cart/StoreCart/StoreCartEndpoint.cs
@@ -1,6 +1,27 @@
 ﻿namespace Cart.API.Cart.StoreCart
 {
-    public class StoreCartEndpoint
+    public record StoredCartRequest(ShoppingCart Cart);
+    public record StoreCartResponse(string UserName);
+
+    public class StoreCartEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/carrinho", async (StoredCartRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<StoreCartCommand>();
+
+                var result = await sender.Send(command);
+
+                var response = result.Adapt<StoreCartResponse>();
+
+                return Results.Created($"/carrinho/{response.UserName}", response);
+            })
+            .WithName("CreateProduct")
+            .Produces<StoreCartResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create Product")
+            .WithDescription("Create Product");
+        }
     }
 }
