27f802be9e2d62f6be23598f82dbdfc484929778
Lucas M. Angelo <angelucas@gmail.com>
2024-07-04 23:07:28 -0300

    - Add custom exceptions to Cart.API

--- src/Services/Cart/Cart.API/Program.cs
+++ src/Services/Cart/Cart.API/Program.cs
@@ -1,3 +1,5 @@
+using BuildingBlocks.Exceptions.Handler;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var assembly = typeof(Program).Assembly;
@@ -16,9 +18,11 @@ builder.Services.AddMarten(opts =>
 }).UseLightweightSessions();
 
 builder.Services.AddScoped<ICartRepository, CartRepository>();
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 var app = builder.Build();
 
 app.MapCarter();
+app.UseExceptionHandler(optios => { });
 
 app.Run();
