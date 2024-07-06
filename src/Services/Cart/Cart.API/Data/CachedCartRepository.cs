e703f137674ccb563d7c3bb745c944d5c51d5f63
Lucas M. Angelo <angelucas@gmail.com>
2024-07-05 23:14:32 -0300

    - Add Cart.API Cache repository

---
+++ src/Services/Cart/Cart.API/Data/CachedCartRepository.cs
@@ -0,0 +1,21 @@
+﻿namespace Cart.API.Data
+{
+    public class CachedCartRepository(ICartRepository repository) 
+        : ICartRepository
+    {
+        public async Task<ShoppingCart> GetCart(string userName, CancellationToken cancellationToken = default)
+        {
+            return await repository.GetCart(userName, cancellationToken);
+        }
+
+        public async Task<ShoppingCart> StoreCart(ShoppingCart cart, CancellationToken cancellationToken = default)
+        {
+            return await repository.StoreCart(cart, cancellationToken);
+        }
+
+        public async Task<bool> DeleteCart(string userName, CancellationToken cancellationToken = default)
+        {
+            return await repository.DeleteCart(userName, cancellationToken);
+        }
+    }
+}
