c641fc383df41287e6d5b8a3a28d4cce887d43cd
Lucas M. Angelo <angelucas@gmail.com>
2024-07-06 21:51:05 -0300

    - Implement cart repository cache to Cart.API

--- src/Services/Cart/Cart.API/Data/CachedCartRepository.cs
+++ src/Services/Cart/Cart.API/Data/CachedCartRepository.cs
@@ -1,21 +1,39 @@
-﻿namespace Cart.API.Data
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Cart.API.Data
 {
-    public class CachedCartRepository(ICartRepository repository) 
+    public class CachedCartRepository
+        (ICartRepository repository, IDistributedCache cache) 
         : ICartRepository
     {
         public async Task<ShoppingCart> GetCart(string userName, CancellationToken cancellationToken = default)
         {
-            return await repository.GetCart(userName, cancellationToken);
+            var cacheCart = await cache.GetStringAsync(userName, cancellationToken);
+            if (!string.IsNullOrEmpty(cacheCart)) 
+                return JsonSerializer.Deserialize<ShoppingCart>(cacheCart)!;
+
+            var cart = await repository.GetCart(userName, cancellationToken);
+            await cache.SetStringAsync(userName, JsonSerializer.Serialize(cart));
+            return cart;
         }
 
         public async Task<ShoppingCart> StoreCart(ShoppingCart cart, CancellationToken cancellationToken = default)
         {
-            return await repository.StoreCart(cart, cancellationToken);
+            await repository.StoreCart(cart, cancellationToken);
+
+            await cache.SetStringAsync(cart.UserName, JsonSerializer.Serialize(cart), cancellationToken);
+
+            return cart;
         }
 
         public async Task<bool> DeleteCart(string userName, CancellationToken cancellationToken = default)
         {
-            return await repository.DeleteCart(userName, cancellationToken);
+            await repository.DeleteCart(userName, cancellationToken);
+
+            await cache.RemoveAsync(userName, cancellationToken);
+
+            return true;
         }
     }
 }
