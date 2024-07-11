9fa914ff616804b76825ea6ff780abbdf145dde8
Lucas M. Angelo <angelucas@gmail.com>
2024-07-10 23:23:30 -0300

    feat: add DiscountContext class for EF Core integration

---
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -0,0 +1,16 @@
+﻿using Discount.Grpc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
+{
+    public class DiscountContext : DbContext
+    {
+        public DbSet<Coupon> Coupons { get; set; } = default!;
+
+        public DiscountContext(DbContextOptions<DiscountContext>options)
+            : base(options)
+        {
+
+        }
+    }
+}
