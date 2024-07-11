791dafaac812020510d1ef345d4decaefb088e37
Lucas M. Angelo <angelucas@gmail.com>
2024-07-10 23:03:17 -0300

    feat: add DiscountService class with gRPC methods
    - Added DiscountService class implementing DiscountProtoService.DiscountProtoServiceBase
    - Included methods for GetDiscount, CreateDiscount, UpdateDiscount, and DeleteDiscount

---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,27 @@
+﻿using Grpc.Core;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+    {
+        public override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            return base.GetDiscount(request, context);
+        }
+
+        public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+        {
+            return base.CreateDiscount(request, context);
+        }
+
+        public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+        {
+            return base.UpdateDiscount(request, context);
+        }
+
+        public override Task<DeleteDiscounResponse> DeleteDiscount(DeleteDiscounRequest request, ServerCallContext context)
+        {
+            return base.DeleteDiscount(request, context);
+        }
+    }
+}
