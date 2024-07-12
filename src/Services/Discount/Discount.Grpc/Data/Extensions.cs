4b6f4cfc9ca6987eebc0dd02d5816b04de8606f2
Lucas M. Angelo <angelucas@gmail.com>
2024-07-11 23:04:07 -0300

    feat: add extension method for applying migrations

---
+++ src/Services/Discount/Discount.Grpc/Data/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
+{
+    public static class Extensions
+    {
+        public static IApplicationBuilder UseMigrations(this IApplicationBuilder app) 
+        {
+            using var scope = app.ApplicationServices.CreateScope();
+            using var dbContext = scope.ServiceProvider.GetRequiredService<DiscountContext>();
+            dbContext.Database.MigrateAsync();
+
+           return app;
+        }
+    }
+}
