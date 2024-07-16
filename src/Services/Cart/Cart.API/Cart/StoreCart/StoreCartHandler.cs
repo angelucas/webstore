57ecf9864addee3e01d66228e2daf095cba94a31
Lucas M. Angelo <angelucas@gmail.com>
2024-07-15 21:07:14 -0300

    feat: add DeductDiscount method to apply discounts to shopping cart

--- src/Services/Cart/Cart.API/Cart/StoreCart/StoreCartHandler.cs
+++ src/Services/Cart/Cart.API/Cart/StoreCart/StoreCartHandler.cs
@@ -1,4 +1,6 @@
-﻿namespace Cart.API.Cart.StoreCart
+﻿using Discount.Grpc;
+
+namespace Cart.API.Cart.StoreCart
 {
     public record StoreCartCommand(ShoppingCart Cart) : ICommand<StoreCartResult>;
     public record StoreCartResult(string UserName);
@@ -10,16 +12,26 @@
             RuleFor(x => x.Cart.UserName).NotEmpty().WithName("UserName is required");
         }
     }
-    public class StoreCartCommandHandler(ICartRepository repository)
+    public class StoreCartCommandHandler
+        (ICartRepository repository, DiscountProtoService.DiscountProtoServiceClient discountProto)
         : ICommandHandler<StoreCartCommand, StoreCartResult>
     {
         public async Task<StoreCartResult> Handle(StoreCartCommand command, CancellationToken cancellationToken)
         {
-            ShoppingCart cart = command.Cart;
+            await DeductDiscount(command.Cart, cancellationToken);
 
             await repository.StoreCart(command.Cart, cancellationToken);
 
             return new StoreCartResult(command.Cart.UserName);
+        }
+
+        private async Task DeductDiscount(ShoppingCart cart, CancellationToken cancellationToken)
+        {
+            foreach (var item in cart.Items) 
+            {
+                var coupon = await discountProto.GetDiscountAsync(new GetDiscountRequest { ProductName = item.ProductName }, cancellationToken: cancellationToken);
+                item.Price -= coupon.Amount;
+            }
         }
     }
 }
