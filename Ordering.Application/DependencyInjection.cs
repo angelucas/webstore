c191558c5797040efdbc5b0145527f94dd7a0c8d
Lucas M. Angelo <angelucas@gmail.com>
2024-07-19 21:40:59 -0300

    Update Ordering.Application

---
+++ Ordering.Application/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
