7e4c5fedc7c397a9c6606ac6c1874479df0e7414
Lucas M. Angelo <angelucas@gmail.com>
2024-07-20 19:19:59 -0300

    Add IDomainEvent interface to handle domain events in Ordering.Domain.Abstractions

---
+++ Ordering.Domain/Abstractions/IDomainEvent.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Ordering.Domain.Abstractions
+{
+    public interface IDomainEvent : INotification
+    {
+        Guid EventId => Guid.NewGuid();
+        public DateTime OccurredOn { get; set; }
+        public string EventType => GetType().AssemblyQualifiedName;
+    }
+}
