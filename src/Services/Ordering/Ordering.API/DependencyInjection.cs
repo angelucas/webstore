43eee818fd8bbd4fc3c61d282b454677f682e2de
Lucas M. Angelo <angelucas@gmail.com>
2024-07-19 21:43:28 -0300

    Update Ordering.API

---
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿namespace Ordering.API
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static WebApplication UseApiServices(this WebApplication app)
+        {
+            return app;
+        }
+    }
+}
--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,4 +1,13 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices();
 
 var app = builder.Build();
 
