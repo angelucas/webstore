466a165c60c886ef26fe58084dce32ba279d8c36
Lucas M. Angelo <angelucas@gmail.com>
2024-07-19 21:41:40 -0300

    Update Ordering.Infrastructure

---
+++ Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices
+            (this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("Database");
+
+            return services;
+        }
+    }
+}
