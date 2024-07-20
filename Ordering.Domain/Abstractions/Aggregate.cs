cc002a26fb8a6814d4211673c6833ef7c308eecb
Lucas M. Angelo <angelucas@gmail.com>
2024-07-20 19:18:42 -0300

    Add IAggregate<T> and IAggregate interfaces to handle aggregates and domain events in Ordering.Domain.Abstractions

---
+++ Ordering.Domain/Abstractions/Aggregate.cs
@@ -0,0 +1,22 @@
+﻿namespace Ordering.Domain.Abstractions
+{
+    public abstract class Aggregate<TId> : Entity<TId>, IAggregate<TId>
+    {
+        private readonly List<IDomainEvent> _domainEvents = new();
+        public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+        public void AddDomainEvent(IDomainEvent domainEvent)
+        {
+            _domainEvents.Add(domainEvent);
+        }
+
+        public IDomainEvent[] ClearDomainEvents()
+        {
+            IDomainEvent[] dequeueEvents = _domainEvents.ToArray();
+            
+            _domainEvents.Clear();
+
+            return dequeueEvents;
+        }
+    }
+}
