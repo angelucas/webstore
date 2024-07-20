7e89466e75b5809a462e7da0b77741e1f8781732
Lucas M. Angelo <angelucas@gmail.com>
2024-07-20 19:18:00 -0300

    Add IEntity<T> and IEntity interfaces to define entity properties in Ordering.Domain

---
+++ Ordering.Domain/Abstractions/IEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace Ordering.Domain
+{
+    public interface IEntity<T> : IEntity
+    {
+        public T Id { get; set; }
+    }
+
+    public interface IEntity
+    {
+        public DateTime? CreatedAt { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime? LastModified { get; set; }
+        public string? LastModifiedBy { get; set; }
+    }
+}
