09d374cf25e25dd0c5d27fd74134d7929c66bf4d
Lucas M. Angelo <angelucas@gmail.com>
2024-07-22 18:25:56 -0300

    Add Payment record to represent payment details in Ordering.Domain.ValueObjects

---
+++ Ordering.Domain/ValueObjects/Payment.cs
@@ -0,0 +1,11 @@
+﻿namespace Ordering.Domain.ValueObjects
+{
+    public record Payment
+    {
+        public string? CardName { get; } = default!;
+        public string CardNumber { get; } = default!;
+        public string Expiration { get; } = default!;
+        public string CVV { get; } = default!;
+        public int PaymentMethod { get; } = default!;
+    }
+}
