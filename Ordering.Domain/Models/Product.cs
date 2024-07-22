5ce9c6dfc4dc9fd8c411ec6cf69f580651598751
Lucas M. Angelo <angelucas@gmail.com>
2024-07-22 18:24:41 -0300

    Add Product class to represent products in Ordering.Domain.Models

---
+++ Ordering.Domain/Models/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace Ordering.Domain.Models
+{
+    public class Product : Entity<Guid>
+    {
+        public string Name { get; private set; } = default!;
+        public decimal Price { get; private set; } = default!;
+    }
+}
