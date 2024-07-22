886387d2f06e07cd0ea0ba06701b47b5bc334ab7
Lucas M. Angelo <angelucas@gmail.com>
2024-07-22 18:25:14 -0300

    Add Address record to represent addresses in Ordering.Domain.ValueObjects

---
+++ Ordering.Domain/ValueObjects/Address.cs
@@ -0,0 +1,13 @@
+﻿namespace Ordering.Domain.ValueObjects
+{
+    public record Address
+    {
+        public string FirstName { get; } = default!;
+        public string LastName { get; } = default!;
+        public string? EmailAddress { get; } = default!;
+        public string AddressLine { get; } = default!;
+        public string Country { get; } = default!;
+        public string State { get; } = default!;
+        public string ZipCode { get; } = default!;
+    }
+}
