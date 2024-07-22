cc94fabe3a0e5a4ad4987601ab7ebe916b171914
Lucas M. Angelo <angelucas@gmail.com>
2024-07-22 18:23:51 -0300

    Add Customer class to represent customers in Ordering.Domain.Models

---
+++ Ordering.Domain/Models/Customer.cs
@@ -0,0 +1,8 @@
+﻿namespace Ordering.Domain.Models
+{
+    public class Customer : Entity<Guid>
+    {
+        public string Name { get; private set; } = default!;
+        public string Email { get; private set; } = default!;
+    }
+}
