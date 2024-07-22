a7d8c99d4d93fb6e1110c05ed502105d9a6b6461
Lucas M. Angelo <angelucas@gmail.com>
2024-07-22 18:23:23 -0300

    Add Order class to represent orders in Ordering.Domain.Models

---
+++ Ordering.Domain/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿namespace Ordering.Domain.Models
+{
+    public class Order : Aggregate<Guid>
+    {
+        private readonly List<OrderItem> _orderItems = new();
+        public IReadOnlyList<OrderItem> OrderItems => _orderItems.AsReadOnly();
+        public Guid CustomerId { get; private set; } = default!;
+        public string OrderName { get; private set; } = default!;
+        public Address ShippingAddress { get; private set; } = default!;
+        public Address BillingAddres { get; private set; } = default!;
+        public Payment Payment { get; private set; } = default!;
+        public OrderStatus Status { get; private set; } = OrderStatus.Pending;
+        public decimal TotalPrice
+        {
+            get => OrderItems.Sum(x => x.Price * x.Quantity);
+            private set { }
+        }
+    }
+}
