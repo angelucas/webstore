4b996f84fc57fc6b989bf29ca38de6fff8488b69
Lucas M. Angelo <angelucas@gmail.com>
2024-07-23 22:01:37 -0300

    feat: add Order aggregate root to Ordering.Domain.Models

--- Ordering.Domain/Models/Order.cs
+++ Ordering.Domain/Models/Order.cs
@@ -1,11 +1,11 @@
 ﻿namespace Ordering.Domain.Models
 {
-    public class Order : Aggregate<Guid>
+    public class Order : Aggregate<OrderId>
     {
         private readonly List<OrderItem> _orderItems = new();
         public IReadOnlyList<OrderItem> OrderItems => _orderItems.AsReadOnly();
-        public Guid CustomerId { get; private set; } = default!;
-        public string OrderName { get; private set; } = default!;
+        public CustomerId CustomerId { get; private set; } = default!;
+        public OrderName OrderName { get; private set; } = default!;
         public Address ShippingAddress { get; private set; } = default!;
         public Address BillingAddres { get; private set; } = default!;
         public Payment Payment { get; private set; } = default!;
