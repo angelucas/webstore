17a8c8da28072e01004022d9f7d6284506f375dc
Lucas M. Angelo <angelucas@gmail.com>
2024-07-23 21:53:52 -0300

    feat: add DomainException class to Ordering.Domain.Exceptions

---
+++ Ordering.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,10 @@
+﻿namespace Ordering.Domain.Exceptions
+{
+    public class DomainException : Exception
+    {
+        public DomainException(string message) 
+            : base($"Domain Exception: \"{message}\" throw from domain Layer.")
+        { 
+        }
+    }
+}
