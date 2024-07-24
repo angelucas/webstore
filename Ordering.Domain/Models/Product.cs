ff739678dd6e8a5680c1faad445c79c884691122
Lucas M. Angelo <angelucas@gmail.com>
2024-07-23 22:02:29 -0300

    feat: add Product entity to Ordering.Domain.Models

--- Ordering.Domain/Models/Product.cs
+++ Ordering.Domain/Models/Product.cs
@@ -1,6 +1,6 @@
 ﻿namespace Ordering.Domain.Models
 {
-    public class Product : Entity<Guid>
+    public class Product : Entity<ProductId>
     {
         public string Name { get; private set; } = default!;
         public decimal Price { get; private set; } = default!;
