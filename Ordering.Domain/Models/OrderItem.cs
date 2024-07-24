0c0458ac3d2792e6afeeef9aaf5189dd9860ab0f
Lucas M. Angelo <angelucas@gmail.com>
2024-07-23 22:02:03 -0300

    feat: add OrderItem entity to Ordering.Domain.Models

--- Ordering.Domain/Models/OrderItem.cs
+++ Ordering.Domain/Models/OrderItem.cs
@@ -1,16 +1,17 @@
 ﻿namespace Ordering.Domain.Models
 {
-    public class OrderItem : Entity<Guid>
+    public class OrderItem : Entity<OrderItemId>
     {
-        internal OrderItem(Guid orderId, Guid productId, int quantity, decimal price)
+        internal OrderItem(OrderId orderId, ProductId productId, int quantity, decimal price)
         {
+            Id = OrderItemId.Of(Guid.NewGuid());
             OrderId = orderId;
             ProductId = productId;
             Quantity = quantity;
             Price = price;
         }
-        public Guid OrderId { get; private set; } = default!;
-        public Guid ProductId { get; private set; } = default!;
+        public OrderId OrderId { get; private set; } = default!;
+        public ProductId ProductId { get; private set; } = default!;
         public int Quantity { get; private set; } = default!;
         public decimal Price { get; private set; } = default!;
     }
