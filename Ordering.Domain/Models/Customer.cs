685bd90b96e3017a5f9419a27121c163dec9dfe4
Lucas M. Angelo <angelucas@gmail.com>
2024-07-23 21:54:40 -0300

    feat: add Customer entity to Ordering.Domain.Models

--- Ordering.Domain/Models/Customer.cs
+++ Ordering.Domain/Models/Customer.cs
@@ -1,8 +1,22 @@
 ﻿namespace Ordering.Domain.Models
 {
-    public class Customer : Entity<Guid>
+    public class Customer : Entity<CustomerId>
     {
         public string Name { get; private set; } = default!;
         public string Email { get; private set; } = default!;
+        public static Customer Create(CustomerId id, string name, string email)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(name);
+            ArgumentException.ThrowIfNullOrWhiteSpace(email);
+
+            var customer = new Customer
+            {
+                Id = id,
+                Name = name,
+                Email = email
+            };
+
+            return customer;
+        }
     }
 }
