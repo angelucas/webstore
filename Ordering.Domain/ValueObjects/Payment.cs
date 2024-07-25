ea9fcfffbdb414043fe5a28c7bb14677db702c2b
Lucas M. Angelo <angelucas@gmail.com>
2024-07-25 20:53:59 -0300

    feat: add Payment value object to Ordering.Domain.Models

--- Ordering.Domain/ValueObjects/Payment.cs
+++ Ordering.Domain/ValueObjects/Payment.cs
@@ -7,5 +7,28 @@
         public string Expiration { get; } = default!;
         public string CVV { get; } = default!;
         public int PaymentMethod { get; } = default!;
+
+        protected Payment()
+        {
+        }
+
+        private Payment(string cardName, string cardNumber, string expiration, string cvv, int paymentMethod)
+        {
+            CardName = cardName;
+            CardNumber = cardNumber;
+            Expiration = expiration;
+            CVV = cvv;
+            PaymentMethod = paymentMethod;
+        }
+
+        public static Payment Of(string cardName, string cardNumber, string expiration, string cvv, int paymentMethod)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(cardName);
+            ArgumentException.ThrowIfNullOrWhiteSpace(cardNumber);
+            ArgumentException.ThrowIfNullOrWhiteSpace(cvv);
+            ArgumentOutOfRangeException.ThrowIfGreaterThan(cvv.Length, 3);
+
+            return new Payment(cardName, cardNumber, expiration, cvv, paymentMethod);
+        }
     }
 }
