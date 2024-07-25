c7b158e8bee07a33426b817c51e439227bf246c9
Lucas M. Angelo <angelucas@gmail.com>
2024-07-24 23:53:35 -0300

    feat: add OrderId value object to Ordering.Domain.ValueObjects

--- Ordering.Domain/ValueObjects/OrderId.cs
+++ Ordering.Domain/ValueObjects/OrderId.cs
@@ -3,5 +3,17 @@
     public record OrderId
     {
         public Guid Value { get; }
+        private OrderId(Guid value) => Value = value;
+
+        public static OrderId Of(Guid value) 
+        {
+            ArgumentNullException.ThrowIfNull(value);
+            if (value == Guid.Empty)
+            {
+                throw new DomainException("OrderId cannot be empty.");
+            }
+
+            return new OrderId(value);
+        }
     }
 }
