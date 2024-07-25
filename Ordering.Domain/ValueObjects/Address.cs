49b6b5279aa17fac81c8384488c8d817a439f98e
Lucas M. Angelo <angelucas@gmail.com>
2024-07-25 20:53:39 -0300

    feat: add Address value object to Ordering.Domain.Models

--- Ordering.Domain/ValueObjects/Address.cs
+++ Ordering.Domain/ValueObjects/Address.cs
@@ -9,5 +9,27 @@
         public string Country { get; } = default!;
         public string State { get; } = default!;
         public string ZipCode { get; } = default!;
+        protected Address()
+        {
+        }
+
+        private Address(string firstName, string lastName, string emailAddres, string addresLine, string country, string state, string xipCode)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            EmailAddress = emailAddres;
+            AddressLine = addresLine;
+            Country = country;
+            State = state;
+            ZipCode = xipCode;
+        }
+
+        public static Address Of(string firstName, string lastName, string emailAddres, string addresLine, string country, string state, string xipCode)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(emailAddres);
+            ArgumentException.ThrowIfNullOrWhiteSpace(firstName);
+
+            return new Address(firstName, lastName, emailAddres, addresLine, country, state, xipCode);
+        }
     }
 }
