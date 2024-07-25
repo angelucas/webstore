4f473e2197d4eab14e5c78caf6dcc43d8e8fefa5
Lucas M. Angelo <angelucas@gmail.com>
2024-07-24 23:54:20 -0300

    feat: add OrderName value object to Ordering.Domain.ValueObjects

--- Ordering.Domain/ValueObjects/OrderName.cs
+++ Ordering.Domain/ValueObjects/OrderName.cs
@@ -2,6 +2,16 @@
 {
     public record OrderName
     {
-        public string Value { get; set; }
+        private const int DefaultLenght = 5;
+        public string Value { get; }
+        private OrderName(string value) => Value = value;
+
+        public static OrderName Of(string value)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(value);
+            ArgumentOutOfRangeException.ThrowIfNotEqual(value.Length, DefaultLenght);
+
+            return new OrderName(value);
+        }
     }
 }
