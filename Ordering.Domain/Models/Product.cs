3615717e021e1d0b0ea393c2af2513fbe8082685
Lucas M. Angelo <angelucas@gmail.com>
2024-07-29 21:49:14 -0300

    feat: add Create method to Product class in Ordering.Domain.Models

--- Ordering.Domain/Models/Product.cs
+++ Ordering.Domain/Models/Product.cs
@@ -4,5 +4,19 @@
     {
         public string Name { get; private set; } = default!;
         public decimal Price { get; private set; } = default!;
+        public static Product Create(ProductId id, string name, decimal price)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(name);
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+            var product = new Product
+            {
+                Id = id,
+                Name = name,
+                Price = price
+            };
+
+            return product;
+        }
     }
 }
