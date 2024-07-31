7bff083ed4a292a5499a6ef74c3366ef66112c8b
Lucas M. Angelo <angelucas@gmail.com>
2024-07-30 21:52:04 -0300

    feat: add Create, Update, Add, and Remove methods to Order class in Ordering.Domain.Models

--- Ordering.Domain/Models/Order.cs
+++ Ordering.Domain/Models/Order.cs
@@ -15,5 +15,52 @@
             get => OrderItems.Sum(x => x.Price * x.Quantity);
             private set { }
         }
+
+        public static Order Create(OrderId id, CustomerId customerId, OrderName orderName, Address shippingAddres, Address billingAddress, Payment payment)
+        {
+            var order = new Order
+            {
+                Id = id,
+                CustomerId = customerId,
+                OrderName = orderName,
+                ShippingAddress = shippingAddres,
+                BillingAddres = billingAddress,
+                Payment = payment,
+                Status = OrderStatus.Pending,
+            };
+
+            order.AddDomainEvent(new OnderCreatedEvent(order));
+
+            return order;
+        }
+
+        public void Update(OrderName orderName, Address shippingAddres, Address billingAddress, Payment payment, OrderStatus status)
+        {
+            OrderName = orderName;
+            ShippingAddress = shippingAddres;
+            BillingAddres = billingAddress;
+            Payment = payment;
+            Status = status;
+
+            AddDomainEvent(new OrderUpdatedEvent(this));
+        }
+
+        public void Add(ProductId productId, int quantity, decimal price)
+        {
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(quantity);
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+            var orderItem = new OrderItem(Id, productId, quantity, price);
+            _orderItems.Add(orderItem);
+        }
+
+        public void Remove(ProductId productId)
+        {
+            var orderItem = _orderItems.FirstOrDefault(x => x.ProductId == productId);
+            if (orderItem is not null)
+            {
+                _orderItems.Remove(orderItem);
+            }
+        }
     }
 }
