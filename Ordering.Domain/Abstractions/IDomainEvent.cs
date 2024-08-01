b8cfe35a9f5ed014cabf1facb23872fc61ac64b4
Lucas M. Angelo <angelucas@gmail.com>
2024-07-31 22:00:20 -0300

    feat: add OccurredOn property to domain events with DateTime in UTC

--- Ordering.Domain/Abstractions/IDomainEvent.cs
+++ Ordering.Domain/Abstractions/IDomainEvent.cs
@@ -5,7 +5,7 @@ namespace Ordering.Domain.Abstractions
     public interface IDomainEvent : INotification
     {
         Guid EventId => Guid.NewGuid();
-        public DateTime OccurredOn { get; set; }
+        public DateTime OccurredOn => DateTime.UtcNow;
         public string EventType => GetType().AssemblyQualifiedName;
     }
 }
