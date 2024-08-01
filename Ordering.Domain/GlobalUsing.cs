73220de8720474646f5853a6ebe496d11116d823
Lucas M. Angelo <angelucas@gmail.com>
2024-07-31 22:02:21 -0300

    feat: add global using statement for Ordering.Domain.Events

--- Ordering.Domain/GlobalUsing.cs
+++ Ordering.Domain/GlobalUsing.cs
@@ -3,3 +3,4 @@ global using Ordering.Domain.Models;
 global using Ordering.Domain.Enums;
 global using Ordering.Domain.ValueObjects;
 global using Ordering.Domain.Exceptions;
+global using Ordering.Domain.Events;
