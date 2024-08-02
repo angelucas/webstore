bed94f9e4bbe037ebd2f55a2723632a9cfa2d363
Lucas M. Angelo <angelucas@gmail.com>
2024-08-01 21:43:17 -0300

    feat: add Order entity configuration placeholder for EF Core

---
+++ Ordering.Infrastructure/Data/Configurations/OrderConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Models;
+
+namespace Ordering.Infrastructure.Data.Configurations
+{
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
