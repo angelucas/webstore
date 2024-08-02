2433652671ed0caeaa0abdfdef9b07ee3022fea2
Lucas M. Angelo <angelucas@gmail.com>
2024-08-01 21:43:03 -0300

    feat: add OrderItem entity configuration placeholder for EF Core

---
+++ Ordering.Infrastructure/Data/Configurations/OrderItemConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Models;
+
+namespace Ordering.Infrastructure.Data.Configurations
+{
+    public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+    {
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
