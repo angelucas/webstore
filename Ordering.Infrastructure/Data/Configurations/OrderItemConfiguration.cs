bea7e72a6649302d6a60ed353e78e44ee2633cd5
Lucas M. Angelo <angelucas@gmail.com>
2024-08-05 23:00:11 -0300

    feat: configure OrderItem entity with conversions and relationships

--- Ordering.Infrastructure/Data/Configurations/OrderItemConfiguration.cs
+++ Ordering.Infrastructure/Data/Configurations/OrderItemConfiguration.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Ordering.Domain.Models;
+using Ordering.Domain.ValueObjects;
 
 namespace Ordering.Infrastructure.Data.Configurations
 {
@@ -9,6 +10,18 @@ namespace Ordering.Infrastructure.Data.Configurations
         public void Configure(EntityTypeBuilder<OrderItem> builder)
         {
             builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id).HasConversion(
+                                     orderItemId => orderItemId.Value,
+                                     dbId => OrderItemId.Of(dbId));
+
+            builder.HasOne<Product>()
+                .WithMany()
+                .HasForeignKey(x => x.ProductId);
+
+            builder.Property(x => x.Quantity).IsRequired();
+
+            builder.Property(x => x.Price).IsRequired();
         }
     }
 }
