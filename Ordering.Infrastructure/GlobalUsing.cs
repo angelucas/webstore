f9d1bd0710e6e219e3703a849b573c6ab0858989
Lucas M. Angelo <angelucas@gmail.com>
2024-08-09 23:58:33 -0300

    chore: add global using directives for common namespaces in Ordering project

---
+++ Ordering.Infrastructure/GlobalUsing.cs
@@ -0,0 +1,8 @@
+﻿global using Microsoft.EntityFrameworkCore;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Ordering.Infrastructure.Data;
+global using Ordering.Domain.Models;
+global using System.Reflection;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using Ordering.Domain.ValueObjects;
