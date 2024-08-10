d84d4c5afb67bdced2ef74f06cdbe6aea870592c
Lucas M. Angelo <angelucas@gmail.com>
2024-08-10 00:00:43 -0300

    refactor: removed local usings from CustomerConfiguration and moved them to global usings

--- Ordering.Infrastructure/Data/Configurations/CustomerConfiguration.cs
+++ Ordering.Infrastructure/Data/Configurations/CustomerConfiguration.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using Ordering.Domain.Models;
-using Ordering.Domain.ValueObjects;
+﻿
+
 
 namespace Ordering.Infrastructure.Data.Configurations
 {
